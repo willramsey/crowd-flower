6e9239d04e6980ecdd80df112b7b40a6435f2c98
William Ramsey <wramsey@gmail.com>
2017-11-04 13:55:51 -0700

    Remove unnecessary boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        CrowdFlower
+      </div>
+    );
+  }
+}
+
+export default App;
